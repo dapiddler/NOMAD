872f98685152cf4035c5575dda95130db3f5d000
piddler <piddlerintheroot@gmail.com>
2017-04-18 05:54:31 +0000

    template

--- web/webserver.py
+++ web/webserver.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask import render_template
+import config
+
 app = Flask(__name__)
 
 @app.route("/")
+def main():
+
+	print("config details")
+
+	print(config.LOCAL_DNS_PORT)
+
+	return str(config.LOCAL_DNS_PORT)
+
+@app.route("/hello/")
 def hello():
-    return "Hello World!"
+	return render_template('index.html')
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True, host = str(config.LOCAL_DNS_HOST))
